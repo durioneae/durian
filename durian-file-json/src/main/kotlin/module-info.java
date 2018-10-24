b40067f15e0083f06111a9833cb43657e34a305b
Paul Smelser <paul.smelser@gmail.com>
2018-10-23 22:05:43 -0400

    removing module-info.java from durian-file-json for now

--- durian-file-json/src/main/kotlin/module-info.java
+++
@@ -1,9 +0,0 @@
-module durian.file.json {
-	requires com.psmelser.durian;
-	requires kotlin.stdlib;
-	requires com.fasterxml.jackson.databind;
-	requires jackson.json;
-	requires json.path;
-
-	exports com.psmelser.durian.file.json;
-}
