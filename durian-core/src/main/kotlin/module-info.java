90e7c5eb527caac94ac40c30b8a77868562b177b
Paul Smelser <paul.smelser@gmail.com>
2018-10-23 22:15:10 -0400

    completely removing modules for now

--- durian-core/src/main/kotlin/module-info.java
+++
@@ -1,6 +0,0 @@
-module com.psmelser.durian {
-	requires kotlin.stdlib;
-
-	exports com.psmelser.durian;
-	exports com.psmelser.durian.file;
-}
--- durian-core/src/test/java/com/psmelser/durian/file/ResourceJavaTest.java
+++ durian-core/src/test/java/com/psmelser/durian/file/ResourceJavaTest.java
@@ -12,21 +12,21 @@ public class ResourceJavaTest {
 
 	@Test
 	public void testFlat() {
-		var file = Resource.Companion.parseFile("multi-line-text.txt");
+		Resource file = Resource.Companion.parseFile("multi-line-text.txt");
 
 		assertThat(file.getFlattenedText()).isEqualTo("This is a testwith more than one line");
 	}
 
 	@Test
 	public void testNoWhite() {
-		var file = Resource.Companion.parseFile("multi-line-text.txt");
+		Resource file = Resource.Companion.parseFile("multi-line-text.txt");
 
 		assertThat(file.getNoWhitespaceText()).isEqualTo("Thisisatestwithmorethanoneline");
 	}
 
 	@Test
 	public void testMultiLine() {
-		var file = Resource.Companion.parseFile("multi-line-text.txt");
+		Resource file = Resource.Companion.parseFile("multi-line-text.txt");
 
 		assertThat(file.getText()).isEqualTo("This is a test\nwith more than one line");
 	}
